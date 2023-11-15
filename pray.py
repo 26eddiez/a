db636c83087504d2f04999796e1f526dad0243e9
26eddiez <26eddiez@students.harker.org>
2023-11-14 23:18:57 -0800

    fusion issue

--- pray.py
+++ pray.py
@@ -34,6 +34,7 @@ from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 
 def create_cnn(width, height, depth, filters=(16, 32, 64)):
+    model = Sequential()
     model.add(Conv2D(64,kernel_size=(3,3), activation = 'relu', input_shape = (320,320,9), kernel_regularizer = 'l2'))
     model.add(MaxPooling2D(pool_size = 2, strides = 2))
     model.add(Conv2D(32,kernel_size=(3,3),activation = 'relu'))
